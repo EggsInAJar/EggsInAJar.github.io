521a81a0cbfec43edc4bc39c372302285b2ca65b
EggsInAJar <66280529+EggsInAJar@users.noreply.github.com>
2022-11-17 02:19:13 -0800

    finished hw8

--- homeworks/hw8/assets/js/script.js
+++ homeworks/hw8/assets/js/script.js
@@ -1,23 +1,7 @@
-function averageThreeNumbers(a, b, c) {
-    let sum = a + b + c;
-    let avg = sum / 3;
-    return avg;
+let songsSecondHalf = document.getElementById("songs-second-half")
+
+function toggleSongs() {
+    songsSecondHalf.classList.toggle("hidden");
 }
 
-function createSentence(num, noun) {
-    return `On average a Berkeley student has ${num} ${noun}s`;
-}
-
-function getRandomNum(max) {
-    return Math.floor(Math.random() * max);
-}
-
-let x = getRandomNum(20);
-let y = getRandomNum(10);
-let z = getRandomNum(13);
-
-let avg = averageThreeNumbers(x, y, z);
-
-let sentence = createSentence(avg, "tiger");
-
-console.log(sentence);
+document.getElementById("toggle-button").onclick = toggleSongs;
